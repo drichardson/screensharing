50df3b7f3a4cda23fb3f30bb92c7e9b6225af2af
Doug Richardson <doug@rekt.email>
2020-04-14 16:46:40 -0700

    demo how to use format filter in a differnt way

--- streamvp8.sh
+++ streamvp8.sh
@@ -9,7 +9,7 @@ SCALED_WIDTH=1920
 # Display a rectangle around recorded region of screen
 #SHOW_REGION="-show_region 1"
 
-FRAMERATE=30
+FRAMERATE=5
 KEYFRAME_INTERVAL=2
 GOP=$((FRAMERATE*KEYFRAME_INTERVAL))
 
@@ -23,12 +23,14 @@ ffmpeg \
 	-i desktop \
 	\
 	-filter:v "scale=$SCALED_WIDTH:-1" \
+	-filter:v "format=yuv422p" \
 	\
 	-c:v libvpx \
 	-b:v 10M \
 	-crf 10 \
 	-g $GOP \
-	-pix_fmt yuv422p \
 	-f rtp rtp://localhost:5555
+
+# -pix_fmt yuv422p \
 
 
