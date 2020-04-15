0bfb1bbbd7c0e2cbdf246922b91fe71b1271e056
Doug Richardson <doug@rekt.email>
2020-04-14 20:25:37 -0700

    remove pointless flag

--- playsdp.sh
+++ playsdp.sh
@@ -8,11 +8,6 @@ if [[ -z $SDP ]]; then
 	exit 1
 fi
 
-# zerolatency on the player really helps in the case of x265. In my experiment,
-# it reduced latency from 4 seconds to 2 seconds.
-#ZERO_LATENCY="-tune zerolatency"
-
 ffplay \
 	-protocol_whitelist rtp,file,udp \
-	-i "$SDP" \
-	$ZERO_LATENCY
+	-i "$SDP"
